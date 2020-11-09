2a26f16ef43edb8c10af9f97a084b08e76fe2f9a
Jackli77 <Jack.li17@hotmail.com>
2020-11-09 15:21:44 +0100

    user input

--- ProdCons.c
+++ ProdCons.c
@@ -1,1 +1,17 @@
 #include<stdio.h>
+
+int main(int argc, char *argv[]){
+    //Specifie le nombre de threads demandés par l'utilisateur, mets le nombre de threads par défaut à 2
+    if(atoi(argv[1])>0){
+        Nprod = atoi(argv[1]);
+    }
+    else{
+        Nprod = 2;
+    }
+    if(atoi(argv[2])>0){
+        Ncons = atoi(argv[2]);
+    }
+    else{
+        Ncons = 2;
+    }
+
