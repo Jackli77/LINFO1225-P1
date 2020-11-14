5feaa13ada38a4336413e63eefb30c16b1adb879
LIGU <guang.li@student.uclouvain.be>
2020-11-14 18:04:57 +0100

    OUI

--- prodcons.c
+++ prodcons.c
@@ -1,39 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdio.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <unistd.h>
+#include<pthread.h>
+#include<semaphore.h>
+#include<unistd.h>
 #include<string.h>
 #include<stdbool.h>
-
+#define MAX 1024
 #define N 8
 /*Ce fichier résout le problème des producteurs consommateurs pour un producteur qui génère un nombre aléatoire et un consommateur qui attend quelques cycles*/
 pthread_mutex_t mutex;
 sem_t empty;
 sem_t full;
+int take = 0;
+int give = 0;
 int buffer[N];
 
 void insert_item() {
-    for (int i = 0; i < N; i++) {
-        if (buffer[i] == NULL) {
-            buffer[i] = rand();
-            return;
-        }
-    }
+    buffer[give] = rand();
+    give = (1+give)%8;
 }
 
-void remove_item() {
-    for (int i = 0; i < N; i++) {
-        if (buffer[i] != NULL) {
-            buffer[i] = NULL;
-            return;
-        }
-    }
+int remove_item() {
+    int temp = buffer[take];
+    take = (1+take)%8;
+    return temp;
 }
 
 void producer(void) {
-    while (true) {
+    for(int i = 0;i<MAX;i++) {
         sem_wait(&empty); // attente d'une place libre
         pthread_mutex_lock(&mutex);// section critique
         insert_item();
@@ -43,14 +38,15 @@ void producer(void) {
 }
 
 void consumer(void) {
-    while (true) {
+    int item;
+    for(int i = 0;i<MAX;i++) {
         sem_wait(&full); // attente d'une place remplie
         pthread_mutex_lock(&mutex);// section critique
-        remove_item();
+        int item = remove_item();
+        while (item > RAND_MAX / 10000){}
         pthread_mutex_unlock(&mutex);
         sem_post(&empty); // une place libre en plus
     }
-    while (rand() > RAND_MAX / 10000) {}
 }
 
 int main(int argc, char *argv[]) {
@@ -70,19 +66,17 @@ int main(int argc, char *argv[]) {
     sem_init(&empty, 0, BufferSize);
     sem_init(&full, 0, 0);
 
-    int a[5] = {1, 2, 3, 4, 5}; //Just used for numbering the producer and consumer
-
-    for (int i = 0; i < 5; i++) {
-        pthread_create(&pro[i], NULL, (void *) producer, (void *) &a[i]);
+    for (int i = 0; i < Nprod; i++) {
+        pthread_create(&pro[i], NULL, (void *) producer, i);
     }
-    for (int i = 0; i < 5; i++) {
-        pthread_create(&con[i], NULL, (void *) consumer, (void *) &a[i]);
+    for (int i = 0; i < Ncons; i++) {
+        pthread_create(&con[i], NULL, (void *) consumer, i);
     }
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < Nprod; i++) {
         pthread_join(pro[i], NULL);
     }
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < Ncons; i++) {
         pthread_join(con[i], NULL);
     }
 
