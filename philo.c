02d84d7aab6a6b25d9bc9b6b092f236ccb4eb75c
chlohest <61017549+chlohest@users.noreply.github.com>
2020-11-14 12:14:40 +0100

    Update philo.c

--- philo.c
+++ philo.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 
 
-int PHILOSOPHES;
+int PHILOSOPHES ;
 
 
 pthread_mutex_t *baguette;
@@ -15,19 +15,24 @@ pthread_mutex_t *baguette;
 
 void mange(int id){
 	
-	for(int i = 0; i<rand(); i++){
-		}
-		
+	
 	}
 	
- void* philosophe(void* arg){
+ void* philosophe(void * arg){
+	
+	
 int count = 0;
 int *id = (int *) arg;
-int left = *id;
 
-int right = (left +1)% PHILOSOPHES;
+
+
+int left = (*id);
+
+int right = (left+1)% PHILOSOPHES;
 
 while(count <10000){
+	
+	
 	
 	if(left<right){
 		pthread_mutex_lock(&baguette[left]);
@@ -39,47 +44,52 @@ while(count <10000){
 		pthread_mutex_lock(&baguette[left]);
 		
 		}
+	
 	mange(*id);
+	
 	count ++;
 	pthread_mutex_unlock(&baguette[left]);
 	pthread_mutex_unlock(&baguette[right]);
-		
+	
 	}
 	return (NULL);
 }
 
 int main (int argc, char *argv[]){
-	
-	
 	PHILOSOPHES = 4;
 	if(argc == 2){
 		if(atoi(argv[1])>0){
         		PHILOSOPHES = atoi(argv[1]);
 		}
     	}
+	
+	
     
 	pthread_t phil[PHILOSOPHES];
+	int id[PHILOSOPHES];
+	srand(getpid());
 	baguette = malloc(PHILOSOPHES*sizeof(pthread_mutex_t));
 	
+	if(PHILOSOPHES == 1){
+		for(int i = 0 ; i< 10000;i++){
+			mange(1);}
+			return 1 ;}
 	
 	
 	for(int i= 0 ;i <PHILOSOPHES;i++){
 	pthread_mutex_init(&(baguette[i]),NULL);
+	id[i] = i;
 	}
 	
-	if(PHILOSOPHES == 1){
-		philosophe(&phil[0]);
-		return 0;}
-
-	for(int i=  0; i<PHILOSOPHES;i++){
-		pthread_create(&(phil[i]),NULL,&philosophe,&baguette[i]);
+	for(int i= 0;i<PHILOSOPHES;i++){
+		
+		pthread_create(&(phil[i]),NULL,&philosophe,&(id[i]));
+		
 		}
+	
 		
 		
-		
-		
-		
-	for(int i = PHILOSOPHES-1 ; i>=0;i--){
+	for(int i = 0;i<PHILOSOPHES;i++){
 		pthread_join(phil[i],NULL);
 		
 		}
