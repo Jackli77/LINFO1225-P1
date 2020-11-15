a311e62372917e931d09108a2ec759c60582af4a
chlohest <charles.lohest@student.uclouvain.be>
2020-11-15 16:55:11 +0100

    update

--- Makefile
+++ Makefile
@@ -1,15 +1,22 @@
 CFlags = -std=c99 -g -Wall -Werror
 CC = gcc
 philo: philo.c
-	$(CC) $(CFlags) -o philo.o philo.c -lm -lpthread
+	$(CC) $(CFlags) -o philo philo.c -lm -lpthread
 
 prodcons: prodcons.c
-	$(CC) $(CFlags) -o prodcons.o prodcons.c -lm -lpthread
+	$(CC) $(CFlags) -o prodcons prodcons.c -lm -lpthread
+read: lect_read.c
+	$(CC) $(CFlags) -o read lect_read.c -lm -lpthread
 
 all: philo.c prodcons.c
 	make philo
 	make prodcons
+	make read
 
 clean:
-	rm -f philo.o
-	rm -f prodcons.o
+	rm -f *.o
+	rm -f *.csv
+	rm -f prodcons
+	rm -f philo
+	rm -f read
+
--- script.sh
+++ script.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 RUNS=5
 NTHREAD=8
-echo "nb_coeurs,temps" >script.csv
+echo "nb_coeurs,temps" >philo.csv
+echo "producteur,consommateur,temps" >prodcons.csv
+echo "writer,reader,temps" > read.csv
 for ((i=1; i<RUNS; i++))
 do
     for((n=1; n<=NTHREAD; n++))
     do
-	output=$(/usr/bin/time -f %e ./prodcons.o $n $n -s 2>&1)
-	echo $n,${output} >>script.csv
+       	for((m= 1; m<=NTHREAD; m++))
+	do
+	   prodconsO=$(/usr/bin/time -f %e ./prodcons $n $m -s 2>&1)
+	   echo $n,$m,${prodconsO} >>prodcons.csv  
+	   readO=$(/usr/bin/time -f %e ./read $n $m -s 2>&1)
+	   echo $n,$m,${readO} >>read.csv
+	done
+	philoO=$(/usr/bin/time -f %e ./philo $n -s 2>&1)
+	echo $n,${philoO} >>philo.csv
+
+
+	
+
+	    
     done
     
 done
