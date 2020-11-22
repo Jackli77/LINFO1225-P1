d2b606d64759f6480a7f8911c38c4ae486d68c8c
Jackli77 <Jack.li17@hotmail.com>
2020-11-22 12:49:39 +0100

    tatas

---
+++ tatas.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int lock = 0;
+
+int leave(int mem){
+	
+	asm(
+	"movl %0, %%eax;"
+	"xchgl %%eax,%1;"
+	"movl %%eax,%0;"
+	:"=g"(lock),"=m" (mem)
+	
+	);
+	
+	
+	return mem;
+	}
+	
+int enter(int mem){
+	while(mem == 1){
+		while(lock == 1){}
+	asm(
+	"movl %2 ,%%eax;"
+	"xchgl %%eax,%0;"
+	"movl %%eax, %1;"
+	: "=g"(lock),"=m"(mem)
+	:"m"(mem));
+	printf("block ");
+	}
+	printf("passed ");
+	return mem;
+	}
