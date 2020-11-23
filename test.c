7aa3a697bbf49ddf03f3ba6a7f3929d362aac6d5
chlohest <61017549+chlohest@users.noreply.github.com>
2020-11-23 15:10:26 +0100

    Update test.c

--- test.c
+++ test.c
@@ -1,4 +1,4 @@
-#include "testandset.h"
+#include "tatas.h"
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdio.h>
@@ -12,14 +12,20 @@ int N;
 void section_crit(void){
 	int mem = 1;
 	int count = 0;
+	printf("% d", lock);
 	while(count < 6400/N){
 	mem = enter(mem);
-	while(rand() > RAND_MAX/1000){}
+	while(rand() > RAND_MAX/10000){}
 	mem = leave(mem);
 	count ++;}
 	}
-int main(void){
+int main(int argc, char *argv[]){
 	N = 1;
+	if(argc == 2){
+		if(atoi(argv[1])>0){
+        		N = atoi(argv[1]);
+		}
+    	}
 	pthread_t threads[N];
 	for(int i = 0; i<N; i++){
 		pthread_create(&threads[i],NULL,(void *) section_crit,NULL);
