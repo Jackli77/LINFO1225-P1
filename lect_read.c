4eb6cb38d510c4e6bab347a6af8118e904fc13c3
chlohest <61017549+chlohest@users.noreply.github.com>
2020-11-23 16:08:47 +0100

    Update lect_read.c

--- lect_read.c
+++ lect_read.c
@@ -5,13 +5,13 @@
 #include<string.h>
 #include<pthread.h>
 // initialisation des variables globales
-sem_t mutex,writeblock;
+sem_t writeblock;
 int readercount = 0;
 int writercount = 0;
 int data = 0;
 int count1 = 0;
 int count2 = 0;
-pthread_mutex_t rcounter,wcounter;
+pthread_mutex_t mutex;
 
 //simulation ecriture et lecture
 void ecrire(){
@@ -28,53 +28,41 @@ void lecture(){
 void reader(void* param)
 {
 	while(count2 < 2560){	
-    sem_wait(&mutex);
+   
     //augmentation du lecteur
-    pthread_mutex_lock(&rcounter);
+    pthread_mutex_lock(&mutex);
     
     readercount++;
     
     if(readercount==1)
         sem_wait(&writeblock);
         
-   pthread_mutex_unlock(&rcounter);
+   pthread_mutex_unlock(&mutex);
    
-    sem_post(&mutex);
+    
     //lecture et libération du mutex
     lecture();
-    sem_wait(&mutex);
-    pthread_mutex_lock(&rcounter);
+    
+    pthread_mutex_lock(&mutex);
     readercount--;
     if(readercount==0)
     {
         sem_post(&writeblock);
     }
     //lecture est finie
-    pthread_mutex_unlock(&rcounter);
-    sem_post(&mutex);}
+    pthread_mutex_unlock(&mutex);
+    }
    
 }
 
 void writer(void* param)
 {
 	while(count1 < 640){
-		//blocage du mutex pour ajouter un ecrivain
-   pthread_mutex_lock(&wcounter);
-   writercount++;
-   //si l'écrivain est tout seul il attend
-   if(writercount ==1){sem_wait(&mutex);}
-   
-   pthread_mutex_unlock(&wcounter);
-   //attente de permission d'écriture
-    sem_wait(&writeblock);
+	sem_wait(&writeblock);
     ecrire();
     sem_post(&writeblock);
     //On a fini d'écrire
-    pthread_mutex_lock(&wcounter);
-    writercount --;
-    //l'écrivain se retire 
-    if(writercount == 0){sem_post(&mutex);}
-    pthread_mutex_unlock(&wcounter);
+    
     
     }
     
@@ -96,29 +84,29 @@ int main(int argc, char *argv[])
             M = atoi(argv[2]);
         }
     }
-    sem_init(&mutex,0,1);
+   
     sem_init(&writeblock,0,1);
     pthread_t writerthreads[M];
     pthread_t readerthreads[N];
-    pthread_mutex_init(&rcounter,NULL);
-    pthread_mutex_init(&wcounter,NULL);
-    for(int i = 0; i<M; i++){
-	pthread_create(&writerthreads[i],NULL, (void*)writer,NULL);
-	}
+    pthread_mutex_init(&mutex,NULL);
      for(int i = 0; i<N; i++){
 		pthread_create(&readerthreads[i],NULL, (void*)reader,NULL);
 		}
+		for(int i = 0; i<M; i++){
+	pthread_create(&writerthreads[i],NULL, (void*)writer,NULL);
+	}
 	
   
-	for(int i =0;i<M;i++)
-		{
-			
-			pthread_join(writerthreads[i],NULL);
-		}
     for(int i =0;i<N;i++)
     {
         
         pthread_join(readerthreads[i],NULL);
     }
+    
+	for(int i =0;i<M;i++)
+		{
+			
+			pthread_join(writerthreads[i],NULL);
+		}
  
 }
