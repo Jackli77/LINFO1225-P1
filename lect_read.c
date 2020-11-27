75a08bd6d9132efb4cea44ee46b10bff50b6ac43
chlohest <61017549+chlohest@users.noreply.github.com>
2020-11-27 10:57:55 +0100

    Update lect_read.c

--- lect_read.c
+++ lect_read.c
@@ -1,27 +1,29 @@
-#include<semaphore.h>
+
+#include "mysem.h"
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
 #include<string.h>
 #include<pthread.h>
 // initialisation des variables globales
-sem_t block,readmutex,readcount,writemutex,writecount;
+struct mysem block,readmutex,readcount,writemutex,writecount;
+
 int writercount;
-int readercount ;
+int readercount;
 int count1 = 0;
 int count2 = 0;
 
 
 //simulation ecriture et lecture
-void ecrire(){
-	printf("écrire   ");
+void ecrire(){printf("ecrire ");
+	
 	//on incrémente le conteur d'écriture et lecture
 	
 	while(rand() > RAND_MAX/10000);
 	count1++;
 	}
 void lecture(){
-	printf("lire    ");
+	printf("lire ");
 	
 	while(rand() > RAND_MAX/10000);
 	count2++;}
@@ -30,19 +32,22 @@ void lecture(){
 void reader(void* param)
 {
 	while(count2 < 2560){
-	sem_wait(&block);
-	sem_wait(&readmutex);
-	sem_wait(&readcount);
+	wait(&block);
+	
+	wait(&readmutex);
+	wait(&readcount);
+	fprintf(stdout,"coucou ");
+	fflush(stdout);
 	readercount ++;
-	if(readercount == 1){sem_wait(&writemutex);}
-	sem_post(&readcount);
-	sem_post(&readmutex);
-	sem_post(&block);
+	if(readercount == 1){wait(&writemutex);}
+	post(&readcount);
+	post(&readmutex);
+	post(&block);
 	lecture();
-	sem_wait(&readcount);
+	wait(&readcount);
 	readercount --;
-	if(readercount ==0){sem_post(&writemutex);}
-	sem_post(&readcount);
+	if(readercount ==0){post(&writemutex);}
+	post(&readcount);
 
     }
    
@@ -51,17 +56,17 @@ void reader(void* param)
 void writer(void* param)
 {
 	while(count1 < 640){
-	sem_wait(&writecount);
+	wait(&writecount);
 	writercount++;
-	if(writercount == 1){sem_wait(&readmutex);}
-	sem_post(&writecount);
-	sem_wait(&writemutex);
+	if(writercount == 1){wait(&readmutex);}
+	post(&writecount);
+	wait(&writemutex);
 	ecrire();
-	sem_post(&writemutex);
-	sem_wait(&writecount);
+	post(&writemutex);
+	wait(&writecount);
 	writercount --;
-	if(writercount == 0){sem_post(&readmutex);}
-	sem_post(&writecount);
+	if(writercount == 0){post(&readmutex);}
+	post(&writecount);
     //On a fini d'écrire
     
     
@@ -72,8 +77,8 @@ void writer(void* param)
 
 int main(int argc, char *argv[])
 {
-    int lecteurs = 2;
-    int ecrivains = 4;
+    int lecteurs = 1;
+    int ecrivains = 1;
     
      //Specifie le nombre de threads demandés par l'utilisateur, le nombre de threads par défaut est 2 et 4.
    
@@ -81,22 +86,25 @@ int main(int argc, char *argv[])
         if (atoi(argv[1]) > 0) {
             lecteurs = atoi(argv[1]);
         }
-        if (atoi(argv[2]) > 0) {
+        if (atoi(argv[2]) >= 0) {
             ecrivains = atoi(argv[2]);
         }
     }
 	readercount = lecteurs;
 	writercount = ecrivains;
-    sem_init(&writecount,0,1);
-    sem_init(&writemutex,0,1);
-    sem_init(&readcount,0,1);
-    sem_init(&readmutex,0,1);
-    sem_init(&block,0,1);
+	if(ecrivains == 0){
+		for (int i = 0; i<2560;i++){
+			lecture();}
+		return 0;}
+    my_init(&writecount);
+    my_init(&writemutex);
+    my_init(&readcount);
+    my_init(&readmutex);
+	my_init(&block);
+	
     pthread_t writerthreads[ecrivains];
     pthread_t readerthreads[lecteurs];
-    if(ecrivains == 0){
-		for (int i = 0; i<2560;i++){lecture();}
-		return 0;}
+    
 			
    
    
