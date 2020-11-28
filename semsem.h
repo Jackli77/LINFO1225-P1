f9713488be743aecebcfdee0d4293e480faed754
chlohest <61017549+chlohest@users.noreply.github.com>
2020-11-28 19:50:02 +0100

    Update semsem.h

--- semsem.h
+++ semsem.h
@@ -7,33 +7,35 @@
 #include<stdbool.h>
 #include"mysem.h"
 struct csem{
-	int count;};
+	int count;
 	struct mysem B1;
 	struct mysem B2;
+	};
+
 	
 	void c_init(struct csem *a,int N){
 	a->count = N;
-	my_init(&B1,1);
-	my_init(&B2,0);
+	my_init(&(a->B1),1);
+	my_init(&(a->B2),0);
 	}
 	
 	void wait(struct csem*a){
-		mywait(&B1);
+		mywait(&(a->B1));
 		a->count--;
 		if(a->count<0){
-			printf("block");
-			mypost(&B1);
-			mywait(&B2);
+			printf("block ");
+			mypost(&(a->B1));
+			mywait(&(a->B2));
 		}
-		mypost(&B1);
+		mypost(&(a->B1));
 	}
 	void post(struct csem*a){
-		mywait(&B1);
+		mywait(&(a->B1));
 		a->count++;
 		if(a->count<=0){
-			mypost(&B2);
+			mypost(&(a->B2));
 		}
 		else{
-			mypost(&B1);
+			mypost(&(a->B1));
 		}
 	}
