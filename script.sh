e716cb97d89e1b5cf146fd45e90d4d56ca13346e
chlohest <charles.lohest@student.uclouvain.be>
2020-12-02 19:29:11 +0100

    ok

--- script.sh
+++ script.sh
@@ -16,18 +16,19 @@ do
   
     for((n=1; n<=NTHREAD*2; n++))
     do
-
+	up=($n-$n%2)/2
+	down=($n-$n%2)/2
 	philoO=$(/usr/bin/time -f %e ./philo ${n} -s 2>&1)
 	echo ${n},${philoO} >>philo.csv
 	myphiloO=$(/usr/bin/time -f %e ./myphilo ${n} -s 2>&1)
 	echo ${n},${myphiloO} >>myphilo.csv
-	readO=$(/usr/bin/time -f %e ./read ${n} $n -s 2>&1)
+	readO=$(/usr/bin/time -f %e ./read $up $down -s 2>&1)
 	echo ${n},${readO} >>read.csv
-	myreado=$(/usr/bin/time -f %e ./myread ${n} $n -s 2>&1)
+	myreado=$(/usr/bin/time -f %e ./myread $up $down -s 2>&1)
 	echo ${n},${myreado} >>myread.csv
-	prodo=$(/usr/bin/time -f %e ./prodcons ${n} $n -s 2>&1)
+	prodo=$(/usr/bin/time -f %e ./prodcons $up $down -s 2>&1)
 	echo ${n},${prodo} >>prodcons.csv
-	myprodo=$(/usr/bin/time -f %e ./myprodcons ${n} $n -s 2>&1)
+	myprodo=$(/usr/bin/time -f %e ./myprodcons $up $down -s 2>&1)
 	echo ${n},${myprodo} >>myprodcons.csv
 	taso=$(/usr/bin/time -f %e ./tas ${n} -s 2>&1)
 	echo ${n},${taso} >>tas.csv
